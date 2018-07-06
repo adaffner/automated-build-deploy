57529a36f0a927b5958475eef4d6a6b778c3b143
Arthur Daffner <arthur.daffner@audi.de>
2018-07-06 17:19:04 +0200

    installing maven on jenkins

--- init_groovy/05_install_plugins.groovy
+++ init_groovy/05_install_plugins.groovy
@@ -1,4 +1,13 @@
 import jenkins.model.*
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+a=Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
+b=(a.installations as List);
+b.add(new hudson.tasks.Maven.MavenInstallation("MAVEN3", "/home/jenkins/apache-maven/apache-maven-3.2.3", []));
+a.installations=b
+a.save()
 import java.util.logging.Logger
 
 // list of plugins to be installed
